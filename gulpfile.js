f5b654c0cbfe5a85207960af2d264dbc196f12a7
treindle <treindle@gmail.com>
2015-07-10 12:14:10 -1000

    Modifiying gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,69 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var nodemon = require('gulp-nodemon');
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass( { errLogToConsole: true }))
-    .pipe(gulp.dest('./public/styles'))
-    .pipe(reload({ stream: true }));
+var gulp = require('gulp'),
+  jshint = require('gulp-jshint'),
+  jshintReporter = require('jshint-stylish'),
+  watch = require('gulp-watch'),
+
+// =========================================
+// browsersync
+  nodemon = require('gulp-nodemon'),
+  browserSync = require('browser-sync'),
+  less = require('gulp-less'),
+  reload = browserSync.reload;
+// =========================================
+
+
+/*
+ * Create variables for our project paths so we can change in one place
+ */
+var paths = {
+  'src':['./models/**/*.js','./routes/**/*.js', 'keystone.js', 'package.json']
+};
+
+// // gulp lint
+gulp.task('lint', function(){
+  gulp.src(paths.src)
+    .pipe(jshint())
+    .pipe(jshint.reporter(jshintReporter));
+
 });
 
-gulp.task('serve', ['sass'], function() {
+// // gulp watcher for lint
+gulp.task('watch:lint', function () {
+  gulp.src(paths.src)
+    .pipe(watch())
+    .pipe(jshint())
+    .pipe(jshint.reporter(jshintReporter));
+});
+//
+// BrowserSync
+// ========================================
+//
+// uncomment
+//
+// accordian.less & layout.less | @import "public/bootstrap/bootstrap.less";
+//
+//
+// Compile less into CSS & auto-inject into browsers
+gulp.task('less', function() {
+    return gulp.src("./public/css/**/*.less")
+        .pipe(less({ errLogToConsole: true }))
+        .pipe(gulp.dest("./public/css/browsersync"))
+        .pipe(reload({ stream: true }));
+});
 
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./public/**/*').on('change', reload);
-  gulp.watch('./server/views/**/*.jade').on('change', reload);
+gulp.task('serve', ['less'], function() {
+
+    gulp.watch("./public/css/**/*.less", ['less']);
+    gulp.watch("./templates/**/*.hbs").on('change', reload);
 });
 
 gulp.task('browserSync', function() {
   browserSync({
-    proxy: "localhost:3001"
+    proxy: "localhost:3000",
+    port: 3002
   });
 });
 
-gulp.task('default', ['serve', 'sass', 'browserSync']);
+gulp.task('default', ['serve', 'less', 'browserSync']);
+
+// ========================================
